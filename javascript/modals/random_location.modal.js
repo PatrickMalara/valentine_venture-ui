ab8987174f6aafb0e9b82da062df8b321f5faf4d
PatrickMalara <pat_malara@outlook.com>
2021-11-29 18:08:34 -0500

    Fixed Issue with the Random Location Generator where when, if pagination is removed from a service, then =0 no longer performs a COUNT

--- javascript/modals/random_location.modal.js
+++ javascript/modals/random_location.modal.js
@@ -5,25 +5,28 @@ modals.random_location = {
         // I going to use a random number and search until I find a location
         const count_response = await client.service("locations").find( {
            query: {
-                $limit: 0
+               $select: ['id']
            }
         } );
+        console.log( count_response );
 
-        let random_location_id = Math.floor( Math.random() * count_response.total  + 1);
+        let random_location_id = Math.floor( Math.random() * count_response.length  + 1);
         let random_location_response = undefined;
 
         // Just continue searching until we find a location... I know... but it works for now
         while ( random_location_response == undefined ) { 
+            console.debug( "Searching for a Random Location", random_location_id );
             try {
                 random_location_response = await client.service("locations").get( random_location_id );
             } catch (error) {
-                random_location_id = Math.floor( Math.random() * count_response.total  + 1);
+                random_location_id = Math.floor( Math.random() * count_response.length + 1);
                 random_location_response = undefined;
             }
         }
+        console.debug( "Found a Random Location" );
 
         // Navigate to the Marker on the map and load the location modal for our Random date
-        the_map.flyTo( [random_location_response.latitude, random_location_response.longitude] );
+        the_map.flyTo( [random_location_response.latitude, random_location_response.longitude], 18 );
         modals.open('location');
         modals.location.load_location( random_location_response.id );
 
